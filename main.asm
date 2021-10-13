d42a86756334420b3f3d882124627a82c13e907c
NFuller721 <nfuller7218@gmail.com>
2021-10-13 01:09:58 -0400

    code

--- main.asm
+++ main.asm
@@ -1,19 +1,8 @@
-mov ah, 0x0e
-mov al, 'a'
-int 0x10
-
-mov bl, 0
-
-_loop:
-  inc al 
-  inc bl
-  cmp bl, 30
-  je _exit
+global _start
+_start:
   mov ah, 0x0e
+  mov al, 'a'
   int 0x10
-  jmp _loop
-
-_exit:
   jmp $
 
 times 510-($-$$) db 0
